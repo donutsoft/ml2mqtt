70fadf9be33967a858a20b54f219812086c8311d
donutsoft <paul@murphymail.eu>
2025-04-22 22:33:54 -0700

    Add RandomForest typing

--- ml2mqtt/classifiers/RandomForest.py
+++ ml2mqtt/classifiers/RandomForest.py
@@ -8,35 +8,45 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.tree import export_graphviz
 import logging
 
+from typing import Optional, List, Dict, Any
 from SkillStore import SkillObservation
+
 
 class RandomForest:
     def __init__(self):
-        self.logger = logging.getLogger("ml2mqtt")
-        self._rf_model = None
-        self.labelEncoder = LabelEncoder()
+        self.logger: logging.Logger = logging.getLogger("ml2mqtt")
+        self._rf_model: Optional[RandomForestClassifier] = None
+        self.labelEncoder: LabelEncoder = LabelEncoder()
+        self._modelTrained: bool = False
 
-    def populateDataframe(self, observations: list[SkillObservation]):
-        data = []
-        labels = []
+    def populateDataframe(self, observations: List[SkillObservation]) -> None:
+        data: List[Dict[str, Any]] = []
+        labels: List[str] = []
+
         for observation in observations:
-            label = observation.label
-            sensorValues = observation.sensorValues
-            data.append(sensorValues)
-            labels.append(label)
+            data.append(observation.sensorValues)
+            labels.append(observation.label)
+
+        if not data or not labels:
+            self.logger.warning("No data available for training.")
+            self._modelTrained = False
+            return
+
         X = pd.DataFrame.from_records(data)
         y = self.labelEncoder.fit_transform(labels)
+
         try:
             X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
             rf_model = RandomForestClassifier(n_estimators=50)
             rf_model.fit(X_train, y_train)
             self._rf_model = rf_model
+            self._modelTrained = True
         except ValueError:
             self.logger.info("Not enough data to train the model")
             self._modelTrained = False
 
-    def predictLabel(self, sensorValues):
-        if self._rf_model == None:
+    def predictLabel(self, sensorValues: Dict[str, Any]) -> Optional[str]:
+        if self._rf_model is None:
             return None
 
         X = pd.DataFrame([sensorValues])
